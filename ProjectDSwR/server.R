9aab8c0e3474da1dde35919402a637009dbca5c5
marcmacori <marcmacoril@iqs.url.edu>
2020-07-11 21:10:27 +0200

    Solo he reorganizado las ideas

--- ProjectDSwR/server.R
+++ ProjectDSwR/server.R
@@ -2,5 +2,7 @@
 library(shiny)
 #code
 shinyServer(function(input, output) {
-
+  #Datos
+  ExpArms<-read.csv("arms_exports_us_inflation_adjusted.csv")
+  ImpArms<-read.csv("arms_imports_us_inflation_adjusted.csv")
 })
