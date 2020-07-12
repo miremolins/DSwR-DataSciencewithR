aaa047e6b2b525fae06d9698e0c7cb281b455618
marcmacori <marcmacoril@iqs.url.edu>
2020-07-12 18:50:42 +0200

    Tablas de datos

--- ProjectDSwR/server.R
+++ ProjectDSwR/server.R
@@ -3,6 +3,4 @@ library(shiny)
 #code
 shinyServer(function(input, output) {
   #Datos
-  ExpArms<-read.csv("arms_exports_us_inflation_adjusted.csv")
-  ImpArms<-read.csv("arms_imports_us_inflation_adjusted.csv")
 })
