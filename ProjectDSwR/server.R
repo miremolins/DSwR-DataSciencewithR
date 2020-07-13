5b41376563ab1befb9384b8bfdc10374a717b102
marcmacori <marcmacoril@iqs.url.edu>
2020-07-13 18:11:32 +0200

    cambios varios

--- ProjectDSwR/server.R
+++ ProjectDSwR/server.R
@@ -3,11 +3,5 @@ library(shiny)
 library(readr)
 #code
 shinyServer(function(input, output) {
-  #Datos
-  Childmortality<- read_csv("Datos/child_mortality_0_5_year_olds_dying_per_1000_born.csv")
-  childrenperwoman<-read_csv("Datos/children_per_woman_total_fertility.csv")
-  co2emission<-read_csv("Datos/co2_emissions_tonnes_per_person.csv")
-  incomeperperson<-read_csv("Datos/income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
-  lifeexpectancy<-read_csv("Datos/life_expectancy_years.csv")
-  population<-read_csv("Datos/population_total.csv")
+dfDatos<-merge
 })
--- ProjectDSwR/ui.R
+++ ProjectDSwR/ui.R
@@ -2,4 +2,19 @@
 library(shiny)
 #code
 shinyUI(fluidPage(
+  titlePanel("Data to change your mind"),
+  sidebarLayout(
+    sidebarPanel(
+    actionButton("start","Calculate"),
+    sliderInput("Year","Select year:",value = 1800,min = 1800, max= 2019, step=1, round=0),
+    selectInput("X","Eje X",c("Childmortality","childrenperwoman","co2emission","incomeperperson","lifeexpectancy","population")),
+    selectInput("Y","Eje Y",c("Childmortality","childrenperwoman","co2emission","incomeperperson","lifeexpectancy","population")),
+  ),
+  mainPanel(
+    tabsetPanel(type = "tabs",
+                        tabPanel("Grafic", plotOutput("Plot")),
+                        tabPanel("Dades", dataTableOutput("Table"))
+      )
+    )
+  )
 ))
