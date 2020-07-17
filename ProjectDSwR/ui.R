71c2319bc69d04fbc9bbce24e58b77346b384703
juliamartinezp <juliaa.2619@gmail.com>
2020-07-17 12:14:10 +0200

    cambios en sever 6-54 Explicaciones en 42 cambios en ui.r - selector de graficos(no son los que queremos es solo una prueba) -mas paneles de salida(22)

--- ProjectDSwR/ui.R
+++ ProjectDSwR/ui.R
@@ -1,6 +1,6 @@
 #libraries in use
 library(shiny)
-#code
+#SELECTOR DE OPCIONES
 shinyUI(fluidPage(
   titlePanel("Data to change your mind"),
   sidebarLayout(
@@ -9,12 +9,23 @@ shinyUI(fluidPage(
     sliderInput("Year","Select year:",value = 1800,min = 1800, max= 2019, step=1, round=0),
     selectInput("X","Eje X",c("Childmortality","childrenperwoman","co2emission","incomeperperson","lifeexpectancy","population")),
     selectInput("Y","Eje Y",c("Childmortality","childrenperwoman","co2emission","incomeperperson","lifeexpectancy","population")),
+    radioButtons(inputId = "geom",label = "Chart type",
+                 choices = c("X Y"= "points",
+                             "Boxplot"= "boxplot"),
+                 selected= "points")
   ),
+#CHART+TABLE
+
   mainPanel(
     tabsetPanel(type = "tabs",
                         tabPanel("Grafico", plotOutput("Plot")),
-                        tabPanel("Datos", dataTableOutput("Table"))
+                        tabPanel("DatosX", dataTableOutput("TableX")),
+                        tabPanel("DatosY",dataTableOutput("Table")),
+                tabPanel("Poblacion",dataTableOutput("TableP")),
+                tabPanel("combo",dataTableOutput("TableC"))
+                
       )
     )
   )
 ))
+
