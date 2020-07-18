4caabdbd872f8cb6d51b7d10905736f9a841b642
juliamartinezp <juliaa.2619@gmail.com>
2020-07-18 20:09:22 +0200

    sa

--- ProjectDSwR/server.R
+++ ProjectDSwR/server.R
@@ -1,44 +1,56 @@
 #libraries in use
 library(shiny)
 library(readr)
+
+childmortality1<- read.csv("Datos/child_mortality_0_5_year_olds_dying_per_1000_born.csv")
+childrenperwoman1<- read.csv("Datos/children_per_woman_total_fertility.csv")
+co2emissions1<-read.csv("Datos/co2_emissions_tonnes_per_person.csv")
+incomeperperson1<-read.csv("Datos/income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+lifeexpectancy1<- read.csv("Datos/life_expectancy_years.csv")
+population<-read.csv("Datos/population_total.csv")
 #code
 shinyServer(function(input, output) {
   
   datasetx <- reactive({
     if(input$X=="Childmortality") {
-      datasetx<-child_mortality_0_5_year_olds_dying_per_1000_born[,c(1,input$Year-1798)]
+      datasetx<-childmortality1[,c(1,input$Year-1798)]
     } else if(input$X=="childrenperwoman") {
-      datasetx<-children_per_woman_total_fertility[,c(1,input$Year-1798)]
+      datasetx<-childrenperwoman1[,c(1,input$Year-1798)]
     } else if(input$X=="co2emission") {
-      datasetx<-co2_emissions_tonnes_per_person[,c(1,input$Year-1798)]
+      datasetx<-co2emissions1[,c(1,input$Year-1798)]
     } else if(input$X=="incomeperperson") {
-      datasetx<-income_per_person_gdppercapita_ppp_inflation_adjusted[,c(1,input$Year-1798)]
+      datasetx<-incomeperperson1[,c(1,input$Year-1798)]
     } else if(input$X=="lifeexpectancy") {
-      datasetx<-life_expectancy_years[,c(1,input$Year-1798)]
-    } 
+      datasetx<-lifeexpectancy1[,c(1,input$Year-1798)]
+    }
     
   })
   
   datasety <- reactive({
     if(input$Y=="Childmortality") {
-      datasety<-child_mortality_0_5_year_olds_dying_per_1000_born[,c(1,input$Year-1798)]
+      datasety<-childmortality1[,c(1,input$Year-1798)]
     } else if(input$Y=="childrenperwoman") {
-      datasety<-children_per_woman_total_fertility[,c(1,input$Year-1798)]
+      datasety<-childrenperwoman1[,c(1,input$Year-1798)]
     } else if(input$Y=="co2emission") {
-      datasety<-co2_emissions_tonnes_per_person[,c(1,input$Year-1798)]
+      datasety<-co2emissions1[,c(1,input$Year-1798)]
     } else if(input$Y=="incomeperperson") {
-      datasety<-income_per_person_gdppercapita_ppp_inflation_adjusted[,c(1,input$Year-1798)]
+      datasety<-incomeperperson1[,c(1,input$Year-1798)]
     } else if(input$Y=="lifeexpectancy") {
-      datasety<-life_expectancy_years[,c(1,input$Year-1798)]
+      datasety<-lifeexpectancy1[,c(1,input$Year-1798)]
     } 
     
   })
   
   datasetP<-reactive(
-    datasetP<-population_total[,input$Year-1798]
+    datasetP<-population[,c(1,input$Year-1798)]
   )
   
   datasetc<-
+    
+    
+    
+    
+    
 #el datasetC seria la combinacion de los otros tres para que salga pais+poblacio+variablex+
     #variableY todo en una misma tabla , la idea es que si funciona eliminar el output de
     #las otras tablas y dejar esta, pero los datset se quedarian.
