4117680e630bac953a63d037645ecca3c3936e58
marcmacori <marcmacoril@iqs.url.edu>
2020-07-19 14:57:34 +0200

    BASES DEL GRAFICO HECHAS

--- ProjectDSwR/server.R
+++ ProjectDSwR/server.R
@@ -23,39 +23,60 @@ co2emissions1$Years<- as.numeric(as.character(co2emissions1$Years))
 incomeperperson1$Years<- as.numeric(as.character(incomeperperson1$Years))
 lifeexpectancy1$Years<- as.numeric(as.character(lifeexpectancy1$Years))
 population1$Years<- as.numeric(as.character(population1$Years))
-
-gapdata <- left_join(childmortality1, childrenperwoman1, by=c("country","Years"))
+countrydata<-read.csv("https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv",stringsAsFactors = FALSE, check.names = FALSE)
+colnames(countrydata)[1] <- c("country")
+colnames(countrydata)[6] <- c("Region")
+colnames(countrydata)[7] <- c("Sub.Region")
+countrydata<-transmute(countrydata,country, Region, Sub.Region)
+countrydata[27,1]<-"Bolivia"
+countrydata[34,1]<-"Brunei"
+countrydata[38,1]<-"Cape Verde"
+countrydata[52,1]<-"Congo, Dem. Rep."
+countrydata[51,1]<-"Congo, Rep."
+countrydata[55,1]<-"Cote d'Ivoire"
+countrydata[60,1]<-"Czech Republic"
+countrydata[106,1]<-"Iran"
+countrydata[122,1]<-"Kyrgyz Republic"
+countrydata[123,1]<-"Lao"
+countrydata[145,1]<-"Micronesia, Fed. Sts."
+countrydata[146,1]<-"Moldova"
+countrydata[119,1]<-"North Korea"
+countrydata[171,1]<-"Palestine"
+countrydata[184,1]<-"Russia"
+countrydata[203,1]<-"Slovak Republic"
+countrydata[250,]<-list("South Korea","Asia","Eastern Asian")
+countrydata[188,1]<-"St. Kitts and Nevis"
+countrydata[189,1]<-"St. Lucia"
+countrydata[192,1]<-"St. Vincent and the Grenadines"
+countrydata[71,1]<-"Swaziland"
+countrydata[217,1]<-"Syria"
+countrydata[220,1]<-"Tanzania"
+countrydata[235,1]<-"United Kingdom"
+countrydata[236,1]<-"United States"
+countrydata[241,1]<-"Venezuela"
+countrydata[242,1]<-"Vietnam"
+#UnirContinentesZonasyVariables
+gapdata <- left_join(population1, childmortality1, by=c("country","Years"))
+gapdata <- left_join(gapdata, childrenperwoman1, by=c("country","Years"))
 gapdata <- left_join(gapdata, co2emissions1, by=c("country","Years"))
 gapdata <- left_join(gapdata, incomeperperson1, by=c("country","Years"))
 gapdata <- left_join(gapdata, lifeexpectancy1, by=c("country","Years"))
-gapdata <- left_join(gapdata, population1, by=c("country","Years"))
-
+gapdata <- left_join(gapdata, countrydata, by=c("country"))
 #code
 shinyServer(function(input, output) {
 #input
-  
 dataset<-reactive ({filter(gapdata, gapdata[,2] == input$Years) })
-
 DrawChart <- reactive({
-  
-  chart <- ggplot(dataset()) 
-  
-  if(input$geom == "points") {
-    chart <- chart + geom_point(aes_string(x = input$X, y = input$Y))
-  } else if(input$geom == "boxplot") {
-    chart <- chart + geom_boxplot(aes(input$Y))
-  } 
-  
+  chart <- ggplot(dataset()) +
+    aes_string(x = input$X, y = input$Y, size=input$size, color = input$color) + 
+    geom_point(alpha=input$transparency) + 
+    scale_size(range=c(1,24))
   print(chart)
-  
 })
-
 #output
-
  output$Table<-renderDataTable(dataset())
  output$Plot <- renderPlot({
    DrawChart()
- }, width = 1200, height = 720)
-
+ })
   })
 
--- ProjectDSwR/ui.R
+++ ProjectDSwR/ui.R
@@ -1,22 +1,26 @@
 ##libraries in use
 library(shiny)
-#SELECTOR DE OPCIONES
+#Interfaz
 shinyUI(fluidPage(
   titlePanel("Data to change your mind"),
+#Opciones de Entrada
   sidebarLayout(
     sidebarPanel(
     sliderInput("Years","Select year:",value = 1800,min = 1800, max= 2019, step=1, round=0),
     selectInput("X","Eje X",c("ChildMortality","ChildrenPerWoman","CO2Emissions","IncomePerPerson","LifeExpectancy","Population")),
     selectInput("Y","Eje Y",c("ChildMortality","ChildrenPerWoman","CO2Emissions","IncomePerPerson","LifeExpectancy","Population")),
+    selectInput('size', 'Size',c("ChildMortality","ChildrenPerWoman","CO2Emissions","IncomePerPerson","LifeExpectancy","Population")),
+    selectInput("color","Color",c("Region","Sub.Region")),
+    sliderInput("transparency","Transparency",value = 0.5, min = 0, max= 1),
     radioButtons(inputId = "geom",label = "Chart type",
-                 choices = c("X Y"= "points",
+                 choices = c("ScatterPlot"= "points",
                              "Boxplot"= "boxplot"),
                  selected= "points")
   ),
-#CHART+TABLE
-
-  mainPanel(
-    tabsetPanel(type = "tabs",
+  
+#Elementos de Salida
+    mainPanel(
+      tabsetPanel(type = "tabs",
                         tabPanel("Grafico", plotOutput("Plot")),
                         tabPanel("Table", dataTableOutput("Table"))
                 
